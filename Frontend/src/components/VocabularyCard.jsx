794197ecd64859fcac0de2fb15f9c3fe301a5d88
Nicholas Amancio <nick.amancio44@gmail.com>
2025-01-18 20:45:37 -0800

    added the card flip animation. I can't believe they took banned tiktok :((( How else am I going to get my dopamine hit

--- Frontend/src/components/VocabularyCard.jsx
+++ Frontend/src/components/VocabularyCard.jsx
@@ -2,25 +2,21 @@ import "../styles/VocabularyCard.css"
 import { useState } from "react"
 
 const VocabularyCard = ( {id, english, japanese, chapter, chapterName} ) => {
-    const [shownWord, setShownWord] = useState(japanese)
+    //const [shownWord, setShownWord] = useState(japanese)
     const [isEnglish, setEnglish] = useState(false)
 
     //toggle between english and japanese words
     const flip = () => {
-        if (!isEnglish) {
-            setShownWord(english)
-            setEnglish(true)
-        }
-        else {
-            setShownWord(japanese)
-            setEnglish(false)
-        }
+        setEnglish(!isEnglish)
     }
 
     return(
         <>
-            <div className={`vocabularyTile chapter-${chapter} ${chapterName}`} id={id} onClick={flip}>
-                <h1>{shownWord}</h1>
+            <div className="flip">
+                <div className={`card chapter-${chapter} ${chapterName} ${isEnglish ? 'is-flipped' : ''}`} id={id} onClick={flip}>
+                    <div className="vocabularyTile front" ><h1>{japanese}</h1></div>
+                    <div className="vocabularyTile back"><h1>{english}</h1></div>
+                </div>
             </div>
         </>
     )
